8cf096be63d02626269071cf421d8751aa4cf250
Lindsey Howard <codelikeagirl91@gmail.com>
2022-06-15 20:47:39 -0400

    handling auth status through redux

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,5 +1,10 @@
 import { combineReducers } from "redux";
+import { reducer as formReducer } from "redux-form";
+import authReducer from "./authReducer";
+import streamReducer from "./streamReducer";
 
 export default combineReducers({
-	replaceMe: () => "asldfkj",
+	auth: authReducer,
+	form: formReducer,
+	streams: streamReducer,
 });
